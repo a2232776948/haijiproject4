9f0cf3066fdbe0b31d8461f98323d9ccf6137b68
xkss <2232776948@qq.com>
2020-02-28 19:02:43 +0800

    fdf

---
+++ src/components/EleTest1.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <el-radio v-model="radio" label="1">备选项</el-radio>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        input: '',
+        radio:''
+      }
+    }
+  }
+</script>
