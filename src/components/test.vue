526f06ea868b20dab47ecb5c9e2c689f785369f2
xujiashu <296299054@qq.com>
2020-03-03 15:29:01 +0800

    test.vue 测试文件

---
+++ src/components/test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <button type="button" class="btn" @click="showPopup">Click me</button>
+    <popup v-show="isPopupVisible" @close="closePopup">
+    </popup>
+  </div>
+</template>
+
+<script>
+import popup from '@/components/popup'
+export default{
+  name: 'app',
+  components: {
+    popup
+  },
+  data (){
+    return {
+      isPopupVisible: false,
+    }
+  },
+  methods: {
+    showPopup(){
+      this.isPopupVisible = true
+    },
+    closePopup(){
+      this.isPopupVisible = false
+    }
+
+  }
+}
+</script>
