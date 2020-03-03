db868c170b4c15b394cec3511b8b7b230b1caa5c
xkss <2232776948@qq.com>
2020-03-03 19:37:26 +0800

    2020/3/3;xkss

---
+++ src/Vtest2.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="v1">
+      <div id="v1-left">
+        <h2>建筑监理用建筑墙体渗水测试分析系统</h2>
+      </div>
+      <div id="v1-right">
+        <i id="i1" class="el-icon-minus"></i>
+        <i id="i2" class="el-icon-full-screen"></i>
+        <i id="i3" class="el-icon-close"></i>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "vtest2"
+    }
+</script>
+
+<style scoped>
+  #v1{
+    height: 30px;
+    width: 1100px;
+    color: black;
+    background: burlywood ;
+    margin: 0 auto;
+  }
+  #v1-left{
+    height: 20px;
+    width: 300px;
+    margin-top: 10px;
+    margin-left: 20px;
+  }
+  #v1-left h2{
+    margin-top: 10px;
+    width: 300px;
+    font-weight: normal;
+    cursor: pointer;
+    font-size: small;
+  }
+  #v1-right{
+    height: 20px;
+    width: 100px;
+    margin-top: -16px;
+    margin-left: 1030px;
+  }
+</style>
