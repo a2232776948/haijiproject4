414e9b26ab94d40c469c32d3da7c9a0e8effabf0
xujiashu <296299054@qq.com>
2020-03-03 16:12:14 +0800

    解决2_3.vue文件冲突

--- src/components/2_3/2_3.vue
+++ src/components/2_3/2_3.vue
@@ -1,8 +1,25 @@
 <template>
   <back-ground active="1">
+    <div class="mainbox">
+        <div style="width: 10px; height: 25px;"></div>
+        <div class="bgbox">
+          <div class="m bor">
+            <h3>建筑墙体基本信息</h3>
+            <div class="s bor">
+              <form action="form_action.asp" method="get">
+                <p>墙体厚度: <input type="text" name="wall" /></p>
+                <p>防水材料: <input type="text" name="prof" /></p>
+                <p>管道类型: <input type="text" name="piptype" /></p>
+              </form>
+            </div>
+          </div>
 
+        </div>
+        <div style="width: 880px; height: 5px;"></div>
+      </div>
   </back-ground>
 </template>
+
 <script>
   import BackGround from "../common/BackGround";
 
@@ -17,6 +34,40 @@
     }
   }
 </script>
+
 <style>
 
+	.mainbox{
+		width: 780px;
+    height: auto;
+		background-color: #E8BF6A;
+	}
+  .bgbox{
+    width: 760px;
+    height: 420px;
+    background-image: url('../../static/images/timg1.jpg');
+    background-repeat: no-repeat;
+    background-size: 760px 415px;
+    margin-left: 10px;
+  }
+
+  .m{
+    width: 280px;
+    height: 410px;
+  }
+  .s{
+    width: 270px;
+    height: 340px;
+    margin: 5px;
+  }
+  .bor{
+    border-style: solid;
+    border-width: 1px;
+  }
+  p{
+    font-weight: bold;
+  }
+  input{
+    background-color: #aaffff;
+  }
 </style>
